5f262e1e55499e8c661fc6366f2a3b62b8ffb501
Wk-P <72795540+Wk-P@users.noreply.github.com>
2022-09-05 12:46:35 +0900

    Add files via upload

--- HU_cpp_file/test.cpp
+++ HU_cpp_file/test.cpp
@@ -7,6 +7,11 @@ typedef struct _Point {
 	int x, y, index, result;
 } _Point;
 
+typedef struct _Array {
+	int size;
+	int* base;
+} _Array;
+
 void bubble_sort_x(_Point* ps, int size) {
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size - i - 1; j++) {
@@ -31,37 +36,52 @@ void bubble_sort_index(_Point* ps, int size) {
 	}
 }
 
-void init_point_set(_Point* ps, int size) {
-	for (int i = 0; i< size;i++) {
-		cout << "x, y = "; cin >> ps[i].x >> ps[i].y;
-		ps[i].result = 0;
-		ps[i].index = i;
+void init_len_arr(_Array* len) {
+	len->base = new int[2000];
+	int i = 0;
+	while (1) {
+		int in = -1;
+		cin >> in;
+		if (!in) break;
+		len->base[i++] = in;
 	}
+	len->size = i;
 }
 
-// test_print
+_Point* init_point_set(_Point* ps, int* size) {
+	ps = new _Point[15];
+	int i = 0;
+	while (cin >> ps[i].x >> ps[i].y) {
+		ps[i].index = i;
+		ps[i].result = 0;
+		i++;
+	}
+	*size = i;
+	return ps;
+}
 
 int main() {
-	int size2 = 10;
-	_Point* ps = new _Point[size2];
-
-	int len[] = {5, 7, 8, 3, 3, 2, 0};
-	int size1 = sizeof(len) / sizeof(len[0]);
+	_Array* len = new _Array;
+	_Point* ps = NULL;
+	init_len_arr(len);
+	int size1 = len->size, size2 = 0;
+	ps = init_point_set(ps, &size2);
 	// 初始化
-	init_point_set(ps, size2);
+
+	cout << "size1 = " << size1 << "size2 = " << size2 << endl;
 
 	int w = 0, h = 0;
 	for (int i = 0; i < size1; i += 2) {
-		w += len[i];
+		w += len->base[i];
 	}
 	for (int i = 1; i < size1; i += 2) {
-		h += len[i];
+		h += len->base[i];
 	}
 
 	// 输入的点按x轴坐标排序
 	bubble_sort_x(ps, size2);
 	//print_ps(ps, size2);
-	for (int q = 0, p = 1, i = 0, k_left = 0, k_right = len[q], hi = h, k = ps[i].x; i < size2;) {
+	for (int q = 0, p = 1, i = 0, k_left = 0, k_right = len->base[q], hi = h, k = ps[i].x; i < size2;) {
 		// test
 		if (ps[i].x == 7 && ps[i].y == 6) {
 			i++;
@@ -92,22 +112,22 @@ int main() {
 			if (ps[i].y > hi) {
 				ps[i].result = -1;
 			}
-			else if (ps[i].y < hi - len[p]) {
+			else if (ps[i].y < hi - len->base[p]) {
 				ps[i].result = 1;
 			}
 			i++;
 			k = ps[i].x;
 			q += 2;
 			k_left = k_right;
-			k_right += len[q];
-			hi = hi - len[p];
+			k_right += len->base[q];
+			hi = hi - len->base[p];
 			p += 2;
 		}
 		else {
 			q += 2;
 			k_left = k_right;
-			k_right += len[q];
-			hi = hi - len[p];
+			k_right += len->base[q];
+			hi = hi - len->base[p];
 			p += 2;
 		}
 	}
